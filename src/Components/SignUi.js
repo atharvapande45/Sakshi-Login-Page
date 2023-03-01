88c105454bab0f15dfb462be6f1de0f6f91b8600
atharvapande45 <atharva.pande20@pccoepune.org>
2023-03-01 15:22:54 +0530

    Complete

---
+++ src/Components/SignUi.js
@@ -0,0 +1,48 @@
+import "./SignUi.css";
+import profile from "./../image/a.jpg";
+import email from "./../image/email.jpg";
+import pass from "./../image/pass.png";
+function SignUi() {
+  return (
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
+          </div>
+          <div>
+            <h1>Signup Page</h1>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input
+                type="text"
+                placeholder="Enter Your Mail"
+                className="name"
+              />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email" />
+              <input
+                type="password"
+                placeholder="Enter Password"
+                className="name"
+              />
+            </div>
+            <div className="login-button">
+              <button>Login</button>
+            </div>
+
+            <p className="link">
+              <a href="#">Forgot password ?</a> Or &nbsp;
+              <a href="../Components/SignUi.js">Sign Up</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUi;
